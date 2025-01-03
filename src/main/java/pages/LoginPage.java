a584ec99dbb3c8d4fa33b072573aefd5c9c0bb13
Michael <michael.r.waheb@gmail.com>
2025-01-03 07:34:20 -0800

    Amazon automation project initiate

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,12 +3,9 @@ package pages;
 import base.BasePage;
 import Models.LoginData;
 import Data.TestData;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -20,7 +17,6 @@ public class LoginPage extends BasePage
     private final By Password = By.id("ap_password");
     private final By ContinueButton = By.id("continue");
     private final By SigninButton = By.id("auth-signin-button");
-    private final By Litepricecurrency = By.id("currency-lite");
     private LoginData loginData;
 
 //Methods to interact
@@ -28,46 +24,28 @@ public class LoginPage extends BasePage
     {
         super(driver);
     }
-    @Step("Select English as site language")
-    public LoginPage clickenglishlanguagebutton()
+
+    @Step("Enter username")
+    //@Test(dataProvider = "LoginData", dataProviderClass = TestData.class)
+    public LoginPage enterusername(String username)
     {
-        if (gettext(language).equals("English"))
-        {
-            click(language);
-        }
+        filldata(Username, username)
+                .click(ContinueButton);
+        return new LoginPage(driver);
+    }
+    @Step("Enter password")
+    public LoginPage enterpassword(String password)
+    {
+        filldata(Password,password);
         return new LoginPage(driver);
     }
 
-    @Step(dataProvider = "LoginData", dataProviderClass = TestData.class)
-    public void Logintest()
+    @Step("Enter password")
+    public LoginPage clicksigninbutton()
     {
-        filldata(Username, loginData.getUsername())
-                .click(ContinueButton)
-                .filldata(Password,loginData.getPassword())
-                .click(SigninButton);
-    }
-
-    @Step("Login")
-    public LoginPage login()
-    {
-
-        Logintest();
+        click(SigninButton);
         return new LoginPage(driver);
     }
 
-    @Step ("check lite package details for country")
-    public LoginPage checklitepackagedetailsforcountries(String Expectedcountryname, String Expectedpackagetype, String price, String currency)
-    {
-        Allure.step("Validate country name");
-        //Validate country name
-        Assert.assertEquals (gettext(countryname),Expectedcountryname);
-        Allure.step("Validate Package type");
-        //Validate Package type
-        Assert.assertEquals(gettext(Litepackage),Expectedpackagetype);
-        Allure.step("Validate Package price & currency");
-        //Validate Package price & currency
-        Assert.assertEquals(gettext(Litepricecurrency),price +" "+ currency + "/month");
-        return new LoginPage(driver);
-    }
 
 }
